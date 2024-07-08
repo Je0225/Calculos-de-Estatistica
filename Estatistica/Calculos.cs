e1fe8e030d9982ab9d8f84515185a8d26332ed4e
Je0225 <jeniferlmattes@gmail.com>
2024-07-08 17:53:48 -0300

    Melhorias

---
+++ Estatistica/Calculos.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Estatistica {
+
+    internal class Calculos {
+
+        private Double Soma { get; }
+
+        public Int32 ValorMinimo { get; }
+
+        public Int32 ValorMaximo { get; }
+
+        private Dictionary<Int32, Int32> Ocorrencias { get; }
+
+        private List<Int32> NumerosOrdenados { get; }
+
+        public Calculos(List<Int32> numerosOrdenados) {
+            NumerosOrdenados = numerosOrdenados;
+
+            ValorMinimo = Int32.MaxValue;
+            ValorMaximo = Int32.MinValue;
+            Ocorrencias = new Dictionary<Int32, Int32>();
+
+            foreach (int numero in NumerosOrdenados) {
+                Soma += numero;
+                if (numero > ValorMaximo) {
+                    ValorMaximo = numero;
+                }
+                if (numero < ValorMinimo) {
+                    ValorMinimo = numero;
+                }
+                if (Ocorrencias.ContainsKey(numero)) {
+                    Ocorrencias[numero] = Ocorrencias[numero] + 1;
+                } else {
+                    Ocorrencias.Add(numero, 1);
+                }
+            }
+        }
+
+        public Int32 CalculaMediana() {
+            Boolean listaEhPar = NumerosOrdenados.Count % 2 == 0;
+            Int32 mediana = 0;
+            Int32 posicaoMediana;
+            if (listaEhPar) {
+                posicaoMediana = (NumerosOrdenados.Count / 2);
+                int posicaoMediana2 = (NumerosOrdenados.Count / 2) + 1;
+                mediana = (NumerosOrdenados[posicaoMediana - 1] + NumerosOrdenados[posicaoMediana2 - 1]) / 2;
+            } else {
+                posicaoMediana = (NumerosOrdenados.Count + 1) / 2;
+                mediana = NumerosOrdenados[posicaoMediana - 1];
+            }
+            return mediana;
+        }
+
+        public String CalculaModa() {
+            String valorModa = "Default";
+            KeyValuePair<Int32, Int32> moda = new KeyValuePair<Int32, Int32>();
+            bool todosIguais = Ocorrencias.Values.All(v => v == Ocorrencias.Values.First());
+
+            if (todosIguais) {
+                valorModa = "Amodal";
+            } else {
+                foreach (KeyValuePair<Int32, Int32> ocorrencia in Ocorrencias) {
+                    if (ocorrencia.Value >= moda.Value) {
+                        if (ocorrencia.Value > moda.Value) {
+                            moda = ocorrencia;
+                            valorModa = moda.Key.ToString();
+                        } else if (ocorrencia.Value == moda.Value) {
+                            valorModa = valorModa + ", " + ocorrencia.Key;
+                        }
+                    }
+                }
+            }
+            return valorModa;
+        }
+
+        public Double CalculaModaDePearson() {
+            return 3 * CalculaMediana() - (2 * CalculaMedia());
+        }
+
+        public Double CalculaMedia() {
+            return Soma / NumerosOrdenados.Count;
+        }
+
+        public Double CalculaVariancia() {
+            Double diferenca = 0;
+            Double media = CalculaMedia();
+            foreach (var num in NumerosOrdenados) {
+                diferenca += Math.Pow(num - media, 2);
+            }
+            return diferenca / NumerosOrdenados.Count;
+        }
+
+        public Double CalculaDesvioPadrao() {
+            return Math.Sqrt(CalculaVariancia());
+        }
+
+    }
+
+}
---
+++ Estatistica/FormPrincipal.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Estatistica {
+
+    public partial class FormPrincipal: Form {
+
+        private List<Int32> numeros { get; set; }
+
+        private List<Int32> numerosOrdenados { get; set; }
+
+        public FormPrincipal() {
+            InitializeComponent();
+            numeros = new List<Int32>();
+            numerosOrdenados = new List<Int32>();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e) {
+            if (!Int32.TryParse(tbNumero.Text.Trim(), out int outroNumero)) {
+                MessageBox.Show(@" não é um valor válido, digite um número inteiro!");
+            } else {
+                Int32 numeroInformado = Convert.ToInt32(tbNumero.Text);
+                lvNumeros.Items.Add(new ListViewItem(new[] { numeroInformado.ToString() }) { Tag = numeroInformado });
+                numeros.Add(numeroInformado);
+            }
+            tbNumero.Clear();
+            tbNumero.Focus();
+        }
+
+        private void btnGerarRol_Click(object sender, EventArgs e) {
+            if (numeros.Count == lvNumerosRol.Items.Count) {
+                MessageBox.Show(@"Os números informados já estão contidos na lista do Rol");
+                return;
+            }
+            numerosOrdenados = numeros.OrderBy(numeroOrdenado => numeroOrdenado).ToList();
+            lvNumerosRol.Items.Clear();
+            foreach (var numero in numerosOrdenados) {
+                lvNumerosRol.Items.Add(new ListViewItem(new[] { numero.ToString() }) { Tag = numero });
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e) {
+            if (numerosOrdenados.Count == 0 && numeros.Count > 0) {
+                MessageBox.Show(@"Clique no botão 'Gerar Rol' antes de calcular os valores");
+                return;
+            }
+            if (numeros.Count == 0) {
+                MessageBox.Show(@"Não há valores para calcular, informe os números e clique em 'Gerar Rol' antes de calcular os valores");
+                return;
+            }
+            PreencheLabels();
+            tbNumero.Focus();
+        }
+
+        private void tbNumero_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+                btnAdicionar.PerformClick();
+            }
+        }
+
+        private void tbNumero_KeyPress(object sender, KeyPressEventArgs e) {
+            e.Handled = !Char.IsDigit(e.KeyChar) && 
+                !e.KeyChar.Equals((Char)Keys.Back) &&
+                !e.KeyChar.Equals((Char)Keys.Enter);
+        }
+
+        private void PreencheLabels() {
+            Calculos calculos = new Calculos(numerosOrdenados);
+
+            lbMaiorNumeroResult.Text = calculos.ValorMaximo.ToString();
+            lbMenorNumeroResult.Text = calculos.ValorMinimo.ToString();
+            lbModaResult.Text = calculos.CalculaModa();
+            lbVarianciaResult.Text = calculos.CalculaVariancia().ToString("F");
+            lbDesvioPadraoResult.Text = calculos.CalculaDesvioPadrao().ToString("F");
+            lbModaPearsonResult.Text = calculos.CalculaModaDePearson().ToString("F");
+            lbMediaResult.Text = calculos.CalculaMedia().ToString("F");
+            lbMedianaResult.Text = calculos.CalculaMediana().ToString("F");
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e) {
+            numeros.Clear();
+            numerosOrdenados.Clear();
+
+            lvNumeros.Items.Clear();
+            lvNumerosRol.Items.Clear();
+            lbMaiorNumeroResult.Text = "";
+            lbMenorNumeroResult.Text = "";
+            lbModaResult.Text = "";
+            lbVarianciaResult.Text = "";
+            lbDesvioPadraoResult.Text = "";
+            lbModaPearsonResult.Text = "";
+            lbMediaResult.Text = "";
+            lbMedianaResult.Text = "";
+        }
+
+    }
+
+}
